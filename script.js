5ef2c4f19b0459cc37b096fc3110a108e7e73d6c
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-07 13:54:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+if (arr.length <= 1) return 0;
+
+    let cost = 0;
+
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b);
+
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let sum = first + second;
+        cost += sum;
+
+        arr.push(sum);
+    }
+
+    return cost;
   
 }
 
